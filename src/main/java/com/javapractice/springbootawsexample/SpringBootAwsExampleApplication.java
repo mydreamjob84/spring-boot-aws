b23164b6969eb120307d56c420cbcd12241316c4
mydreamjob84 <158154828+mydreamjob84@users.noreply.github.com>
2024-04-03 11:48:54 +0530

    first commit

---
+++ src/main/java/com/javapractice/springbootawsexample/SpringBootAwsExampleApplication.java
@@ -0,0 +1,23 @@
+package com.javapractice.springbootawsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAwsExampleApplication {
+	
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "welcome to aws";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsExampleApplication.class, args);
+	}
+
+}
